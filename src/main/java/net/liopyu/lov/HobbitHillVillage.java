5868b67b6108d63d2b47a6a2e106396817986692
Liopyu <mistakensound@gmail.com>
2025-06-21 08:45:03 -0400

    fixed 1.21+ loot tables

--- src/main/java/net/liopyu/lov/HobbitHillVillage.java
+++ src/main/java/net/liopyu/lov/HobbitHillVillage.java
@@ -3,7 +3,7 @@ package net.liopyu.lov;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("hobbit-hill-village")
+@Mod("hobbit_hill_village")
 public class HobbitHillVillage {
     public HobbitHillVillage() {
     }
